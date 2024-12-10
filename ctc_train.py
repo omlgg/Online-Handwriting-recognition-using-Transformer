a389aa131a41c8a64044eb1a6b7218a407d1c1ab
omlgg <minhquan652003@gmail.com>
2024-12-10 16:37:01 +0700

    Update ctc_train.py

--- ctc_train.py
+++ ctc_train.py
@@ -1,6 +1,36 @@
 import torch
 from utils import levenshtein
 from utils import VectorizeChar
+import re
+import jiwer
+
+
+_COMMAND_RE = re.compile(r'\\(mathbb{[a-zA-Z]}|begin{[a-z]+}|end{[a-z]+}|operatorname\*|[a-zA-Z]+|.)')
+
+
+def tokenize_expression(s: str) -> list[str]:
+  r"""Transform a Latex math string into a list of tokens.
+
+  Tokens are strings that are meaningful in the context of Latex
+  e.g. '1', r'\alpha', r'\frac'.
+
+  Args:
+    s: unicode input string (ex: r"\frac{1}{2}")
+
+  Returns:
+    tokens: list of tokens as unicode strings.
+  """
+  tokens = []
+  while s:
+    if s[0] == '\\':
+      tokens.append(_COMMAND_RE.match(s).group(0))
+    else:
+      tokens.append(s[0])
+
+    s = s[len(tokens[-1]) :]
+
+  return tokens
+
 
 vectorizer = VectorizeChar(257)
 
@@ -85,7 +115,26 @@ def train(model, data_loader, optimizer, criterion, device, debug = False, verbo
 
     return epoch_loss / len(data_loader)
 
-def evaluate(model, data_loader, criterion, device, debug = True, padding = 0, kernel_size = 1, stride = 1):
+
+def compute_cer(truth_and_output: list[tuple[str, str]]):
+  """Computes CER given pairs of ground truth and model output."""
+  class TokenizeTransform(jiwer.transforms.AbstractTransform):
+    def process_string(self, s: str):
+      return tokenize_expression(r'{}'.format(s))
+
+    def process_list(self, tokens: list[str]):
+      return [self.process_string(token) for token in tokens]
+
+  ground_truth, model_output = zip(*truth_and_output)
+
+  return jiwer.cer(truth=list(ground_truth),
+            hypothesis=list(model_output),
+            reference_transform=TokenizeTransform(),
+            hypothesis_transform=TokenizeTransform(),
+      )
+
+
+def evaluate(model, data_loader, criterion, device, decoder = greedy_decoder, debug = True, padding = 0, kernel_size = 1, stride = 1):
     printed = not debug
     # works with batch size = 1
     model.eval()
@@ -105,7 +154,7 @@ def evaluate(model, data_loader, criterion, device, debug = True, padding = 0, k
 
         #     loss = criterion(output, tgt)
         #     epoch_loss += loss.item()
-
+        truth_and_output = []
         for batch in data_loader:
             src = batch['src']
             tgt = batch['tgt']
@@ -128,13 +177,14 @@ def evaluate(model, data_loader, criterion, device, debug = True, padding = 0, k
             if not printed:
                 print(tgt[0])
 
+
             for i in range(output.shape[0]):
                 tgt_sentence = "".join([vocab[j] for j in tgt[i]])
                 if not printed:
                     print(tgt_sentence)
                     printed = True
-                total_char += len(tgt_sentence)
-                error_char += levenshtein(greedy_decoder(output[i]), tgt_sentence)
+                pred_sentence = decoder(output[i])
+                truth_and_output.append((tgt_sentence, pred_sentence))
 
             output = output.permute(1, 0, 2) # (seq_len, batch_size, output_dimm)
             loss = criterion(output, tgt, input_lengths, output_lengths)
@@ -145,5 +195,5 @@ def evaluate(model, data_loader, criterion, device, debug = True, padding = 0, k
             del batch['tgt']
             del batch['src_msk']
             del batch['tgt_msk']
-    print(f'CER: {error_char/total_char}')
+    print(f'CER: {compute_cer(truth_and_output)}')
     return epoch_loss / len(data_loader)
